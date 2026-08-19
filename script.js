6fe77324a401f855589e8047da2abb751f612ac9
ellenpalmeiraaa <camargoellen844@gmail.com>
2026-08-19 08:26:57 -0300

    AJUSTE NO HTML

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-function calcularTroco() {
-    const preco = parseFloat(document.getElementById('preco').value);
-    const quantidade = parseInt(document.getElementById('quantidade').value);
-    const dinheiro = parseFloat(document.getElementById('dinheiro').value);
-    const divResultado = document.getElementById('resultado');
+const button = document.querySelector("button")
+const inputPreco = document.getElementById("preco")
+const inputQuantidade = document.getElementById("quantidade")
+const inputDinheiro = document.getElementById("dinheiro")
+const paragrafoResultado = document.getElementById("resultado")
 
-    if (isNaN(preco) || isNaN(quantidade) || isNaN(dinheiro)) {
-        divResultado.textContent = "Por favor, preencha todos os campos.";
-        divResultado.style.color = "red";
-        return;
+button.addEventListener("click", calcularTroco)
+
+function calcularTroco() {
+    if (inputPreco.value === "" || inputQuantidade.value === "" || inputDinheiro.value === "") {
+        alert("Digite um valor válido em todos os campos.")
+        return
     }
 
-    const valorTotal = preco * quantidade;
+    const preco = inputPreco.valueAsNumber
+    const quantidade = inputQuantidade.valueAsNumber
+    const dinheiro = inputDinheiro.valueAsNumber
+
+    const valorTotal = preco * quantidade
 
     if (dinheiro >= valorTotal) {
-        const troco = dinheiro - valorTotal;
-        divResultado.textContent = `TROCO = ${troco.toFixed(2)}`;
-        divResultado.style.color = "black";
+        const troco = dinheiro - valorTotal
+        paragrafoResultado.textContent = `TROCO = ${troco.toFixed(2)}`
     } else {
-        const falta = valorTotal - dinheiro;
-        divResultado.textContent = `DINHEIRO INSUFICIENTE. FALTAM ${falta.toFixed(2)} REAIS`;
-        divResultado.style.color = "red";
+        const falta = valorTotal - dinheiro
+        paragrafoResultado.textContent = `DINHEIRO INSUFICIENTE. FALTAM ${falta.toFixed(2)} REAIS`
     }
 }
